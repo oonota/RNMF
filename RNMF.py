0819008a9a6741a3eeba38c91a507ca74dc13092
oonota <taotaotao0310@gmail.com>
2018-08-18 23:58:26 +0900

    update S

---
+++ RNMF.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+
+class RobustNMF:
+
+
+
+    def initialize_WH(self,X_shape,k):
+
+        W = np.random.random((X_shape[0],k))
+        H = np.random.random((k,X_shape[1]))
+
+        return W,H
+
+
+    def __init__(self,X,k,lamda,maxiter):
+
+        self.X = X
+        self.X_shape = X.shape
+        self.k
+        self.lamda = lamda
+        self.maxiter = maxiter
+
+        self.W , self.H = self.initialize_WH(X.shape,k)
+        self.S = X - np.dot(W,H)
+
+
+    def update_S(self,S,X_shape,lamda):
+
+        for i in range(X_shape[0]):
+            for j in range(X_shape[1]):
+
+                if S[i,j] > (lamda/2):
+                    S[i,j] -= (lamda/2)
+                elif S[i,j] < -(lamda/2):
+                    S[i,j] += (lamda/2)
+                else:
+                    S[i,j] = 0.0
+    
+        return S
+
+
+
+    def rnmf(self):
+
+        X = self.X
+        W = self.W
+        H = self.H
+
+        maxiter = self.maxiter
+        lamda = self.lamda
+
+
+        for iter in range(maxiter):
+
+            S = X - np.dot(W,H)
+            self.update_S(S,X.shape,lamda)
+
+
+
+
+        
